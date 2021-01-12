68c9f6484e6fd6dc0f6c0089a9eeb86071f957d8
unknown <bocchi.dev@gmail.com>
2021-01-12 13:44:56 -0300

    condensing ToDoList and RenderedList files inside the later.
    mend

--- src/components/RenderedList.js
+++ src/components/RenderedList.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
 import './RenderedList.css'
 import ToDo from './ToDo'
@@ -8,8 +7,11 @@ import ToDo from './ToDo'
 
 const RenderedList = ({date}) =>{
 
-    const [ toDoList, setToDoList ] = useState(window.localStorage.getItem('todolist') ? JSON.parse(window.localStorage.getItem('todolist')) : [],);
-
+    const [ toDoList, setToDoList ] = useState(
+        window.localStorage.getItem('todolist')?
+        JSON.parse(window.localStorage.getItem('todolist')) :
+        [],
+    );
 
     useEffect(() => {
         localStorage.setItem('todolist', JSON.stringify(toDoList));
@@ -18,18 +20,20 @@ const RenderedList = ({date}) =>{
     return(
         <div className='renderedlist'>
             <div className='form'>
-                <ToDoForm date={date} toDoList={toDoList} setToDoList={setToDoList}/>
+                <ToDoForm
+                    date={date}
+                    toDoList={toDoList}
+                    setToDoList={setToDoList}
+                 />
             </div>
-            <div>
-            <div>
-            {toDoList.map((todo, id) => {
-                if (todo.deadline === new Date(date).setHours(0,0,0,0)){
-                    return(
-                        <ToDo todo={todo}  key={id} date={date}/>
-                    )
-                };return false;
-            })}         
-        </div> 
+            <div>                
+                    {toDoList.map((todo, id) => {
+                        if (todo.deadline === new Date(date).setHours(0,0,0,0)){
+                            return(
+                                <ToDo todo={todo}  key={id} date={date}/>
+                            )
+                        };return false;
+                    })}
             </div>
         </div>
     )
